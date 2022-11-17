a685cb089d6decf45139295cc8fd56064298edc3
LPFreitas <lfreitasp2001@gmail.com>
2022-11-17 09:26:38 -0300

    Update control files

--- modules/control/alucontrol.vhd
+++ modules/control/alucontrol.vhd
@@ -1,24 +1,20 @@
 entity alucontrol is
     port(
-        aluop: in bit_vector(1 downto 0);
-        opcode: in bit_vector(10 downto 0);
-        aluCtrl: out bit_vector(3 downto 0)
+        funct3   : in  bit_vector (2 downto 0);
+        funct7_5 : in  bit;
+        aluOp    : in  bit_vector (1 downto 0);
+        aluCtrl  : out bit_vector (3 downto 0)
     );
 end entity;
-architecture arch of alucontrol is
-    signal aluCtrlR: bit_vector(3 downto 0);
+
+architecture arch_alucontrol of alucontrol is
+
 begin
 
-    aluCtrlR <= (
-        3 => '0',
-        2 => opcode(9),
-        1 => opcode(3),
-        0 => opcode(8)
-    );
+    aluCtrl <= "0010" when aluOp = "00" or (aluOp = "10" and funct3 = "000" and funct7_5 = '0') else
+               "0110" when aluOp = "01" or (aluOp = "10" and funct3 = "000" and funct7_5 = '1') else
+               "0000" when aluOp = "10" and funct3 = "111" and funct7_5 = '0' else
+               "0001" when aluOp = "10" and funct3 = "110" and funct7_5 = '0' else
+               "1111";
 
-    with aluop select aluCtrl <=
-        "0010"      when "00",
-        "0111"      when "01",
-        aluCtrlR    when others;
-
-end architecture arch;
+end architecture;
