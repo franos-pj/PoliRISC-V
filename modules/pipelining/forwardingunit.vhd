25ad070bd0ed46621e3b0e41463ccf8b23f3f72d
LPFreitas <lfreitasp2001@gmail.com>
2022-11-18 22:44:22 -0300

    Fix and update fowarding unit

--- modules/pipelining/forwardingunit.vhd
+++ modules/pipelining/forwardingunit.vhd
@@ -3,56 +3,55 @@ use ieee.numeric_bit.all;
 
 entity forwardingunit is
     port(
-        idex_Rs1, idex_Rs2: in bit_vector(63 downto 0);
-        exmem_Rd, memwb_Rd: in bit_vector(63 downto 0);
-        exmem_regwrite, memwb_regwrite: in bit;
-        
-        ForwardA, ForwardB: out bit_vector(1 downto 0);
+        exmem_regWrite, memwb_regWrite : in  bit;
+        idex_Rs1, idex_Rs2             : in  bit_vector (4 downto 0);
+        exmem_Rd, memwb_Rd             : in  bit_vector (4 downto 0);
+        forwardA, forwardB             : out bit_vector (1 downto 0)
     );
 end entity;
 
 architecture behavioural of forwardingunit is
 begin
 
-    -- process for ForwardA
-    process(idex_Rs1, exmem_Rd, memwb_Rd, exmem_regwrite, memwb_regwrite)
+    -- process for forwardA
+    process(idex_Rs1, exmem_Rd, memwb_Rd, exmem_regWrite, memwb_regWrite)
     begin
 
-        if ((exmem_regwrite = '1')
+        if ((exmem_regWrite = '1')
             and (to_integer(unsigned(exmem_Rd)) /= 0)
             and (exmem_Rd = idex_Rs1)) then
             
-            ForwardA <= "10";
+            forwardA <= "10";
         
-        elsif ((memwb_regwrite = '1')
+        elsif ((memwb_regWrite = '1')
                and (to_integer(unsigned(memwb_Rd)) /= 0)
                and (memwb_Rd = idex_Rs1)) then
 
-            ForwardA <= "01";
+            forwardA <= "01";
 
-        else ForwardA <= "00";
+        else forwardA <= "00";
         end if;
 
     end process;
 
 
-    -- process for ForwardB
-    process(idex_Rs2, exmem_Rd, memwb_Rd, exmem_regwrite, memwb_regwrite)
+    -- process for forwardB
+    process(idex_Rs2, exmem_Rd, memwb_Rd, exmem_regWrite, memwb_regWrite)
     begin
 
-        if ((exmem_regwrite = '1')
+        if ((exmem_regWrite = '1')
             and (to_integer(unsigned(exmem_Rd)) /= 0)
             and (exmem_Rd = idex_Rs2)) then
             
-            ForwardB <= "10";
+            forwardB <= "10";
         
-        elsif ((memwb_regwrite = '1')
+        elsif ((memwb_regWrite = '1')
                and (to_integer(unsigned(memwb_Rd)) /= 0)
                and (memwb_Rd = idex_Rs2)) then
 
-            ForwardB <= "01";
+            forwardB <= "01";
 
-        else ForwardB <= "00";
+        else forwardB <= "00";
         end if;
 
     end process;
