fa9a2252e973bc40986bdab4f9d087867e79d97c
tomaz-suller <tomaz.suller@usp.br>
2022-11-20 20:10:21 -0300

    Add comments to ALU control

--- modules/control/alucontrol.vhd
+++ modules/control/alucontrol.vhd
@@ -11,10 +11,10 @@ architecture arch_alucontrol of alucontrol is
 
 begin
 
-    aluCtrl <= "0010" when aluOp = "00" or (aluOp = "10" and funct3 = "000" and funct7_5 = '0') else
-               "0110" when aluOp = "01" or (aluOp = "10" and funct3 = "000" and funct7_5 = '1') else
-               "0000" when aluOp = "10" and funct3 = "111" and funct7_5 = '0' else
-               "0001" when aluOp = "10" and funct3 = "110" and funct7_5 = '0' else
+    aluCtrl <= "0010" when aluOp = "00" or (aluOp = "10" and funct3 = "000" and funct7_5 = '0') else -- +
+               "0110" when aluOp = "01" or (aluOp = "10" and funct3 = "000" and funct7_5 = '1') else -- -
+               "0000" when aluOp = "10" and funct3 = "111" and funct7_5 = '0' else -- AND
+               "0001" when aluOp = "10" and funct3 = "110" and funct7_5 = '0' else -- OR
                "1111";
 
 end architecture;
