be35dcca2e5d726c3ded9bb7eab799455dcd16db
tomaz-suller <tomaz.suller@usp.br>
2022-11-28 10:14:22 -0300

    Add assembly and binary test files
    Binaries cannot be assembled directly using a RISC-V assembler due to
    the limited number of supported instructions and the differences between
    the implemented and specified branch instruction.
    
    Implemented branches have their immediates shifted by 2 (rather than 1)
    and are S-format (rather than SB).

--- modules/parameters.vhd
+++ modules/parameters.vhd
@@ -11,7 +11,7 @@ package parameters is
     constant REGISTER_ADDRESS_WIDTH: natural := 5;
 
     constant DAT_BASE_PATH: string := "../software/";
-    constant FOLDER: string := "basic/";
+    constant FOLDER: string := "complete" & "/";
     constant RAM_DAT_FILE: string := DAT_BASE_PATH & FOLDER &
         "ram.dat";
     constant ROM_DAT_FILE: string := DAT_BASE_PATH & FOLDER &
